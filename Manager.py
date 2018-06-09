bc2757056a56fa8870e805979bd259566e6243d5
mxm0 <max.innocentini@gmail.com>
2018-06-09 20:23:52 +0200

    Manager Singleton class pattern

--- Manager.py
+++ Manager.py
@@ -1,2 +1,24 @@
-# Implement manager class with persitency, buffer and all the good stuff
+# Implement manager class with persitency, buffer, Singleton and all the good stuff
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 # Methods needed beginTransaction(), commit(int taid), write(int taid, int pageid, String data)
+class Manager(metaclass=Singleton):
+    def __init__(self, val):
+        # Initialize something if we need to
+
+    def beginTransaction(self,):
+        pass
+
+    def write(self, int taid, int pageid, String data):
+        pass
+    
+    def commit(self, int taid):
+        pass
+
+
+

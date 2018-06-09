0d56f7b3733fe7a94aee823ab58c533dacb5b9c4
mxm0 <max.innocentini@gmail.com>
2018-06-09 20:00:11 +0200

    Set up file structure

---
+++ App.py
@@ -0,0 +1,1 @@
+# App application that start multiple clients in parallel on different threads and concurrently access the manager
---
+++ Manager.py
@@ -0,0 +1,2 @@
+# Implement manager class with persitency, buffer and all the good stuff
+# Methods needed beginTransaction(), commit(int taid), write(int taid, int pageid, String data)

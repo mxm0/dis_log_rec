4b59de568c88de6591a2b56f46449a0588a40eb3
rob brown <rab170@pitt.edu>
2018-06-11 08:48:02 +0200

    add threading and transaction logging

--- App.py
+++ App.py
@@ -1,1 +1,20 @@
 # App application that start multiple clients in parallel on different threads and concurrently access the manager
+
+import uuid
+import random
+import threading
+from Client import Client
+
+n_pages = 3
+n_workers = 5
+
+def worker(i):
+    #pages = [str(uuid.uuid4()).split('-')[0] for _ in range(random.randint(1, n_pages))]
+    pages = [random.randint(1, 500) for _ in range(random.randint(1, n_pages))]
+    c = Client(page_ids=pages)
+    c.execute()
+
+if __name__ == '__main__':
+    pool = [threading.Thread(target=worker, args=(i,)) for i in range(n_workers)]
+    map(lambda t: t.start(), pool)
+
--- Manager.py
+++ Manager.py
@@ -1,3 +1,8 @@
+import uuid
+import logging
+import hashlib
+
+
 # Implement manager class with persitency, buffer, Singleton and all the good stuff
 class Singleton(type):
     _instances = {}
@@ -6,23 +11,36 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+
 # Methods needed beginTransaction(), commit(int taid), write(int taid, int pageid, String data)
-class Manager(metaclass=Singleton):
+class Manager(object):
+    __metaclass__ = Singleton
+
     def __init__(self):
-        # Initialize something if we need to
-      pass
-    
-    def __str__(self):
-      return self.val
+        self.transactions = self.transaction_logger('transactions')
+        self.commits = self.transaction_logger('commits')
 
     def beginTransaction(self):
-      return 1
+        return uuid.uuid4()
 
-    def write(self, taid, pageid, data):
-      pass
-    
+    def write(self, taid, page_id, data):
+        LSN = hashlib.sha256(str([taid, page_id, data]))
+        LSN = LSN.hexdigest()
+        self.transactions.info('{LSN}, {taid}, {page_id}, {data}'.format(LSN=LSN, taid=taid,
+                                                                         page_id=page_id, data=data))
+
     def commit(self, taid):
-      pass
+        # read/recover  commit
+        self.commits.info('{taid}'.format(taid=taid))
 
-
-
+    def transaction_logger(self, file_name):
+        l = logging.getLogger(file_name)
+        l.setLevel(logging.DEBUG)
+        fileHandler = logging.FileHandler(file_name, mode='w+')
+        formatter = logging.Formatter('%(message)s')
+        fileHandler.setFormatter(formatter)
+        l.addHandler(fileHandler)
+        streamHandler = logging.StreamHandler()
+        streamHandler.setFormatter(formatter)
+        l.addHandler(streamHandler)
+        return l

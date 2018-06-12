1177974ed2425e49abd1511c7f3873f99fc14427
mxm0 <max.innocentini@gmail.com>
2018-06-13 01:18:54 +0200

    Done recovery

--- App.py
+++ App.py
@@ -4,10 +4,11 @@ import uuid
 import random
 import threading
 from Client import Client
+from Manager import Manager
+import os
 
 n_pages = 2
 n_workers = 3
-lock = threading.Lock()
 
 def worker():
     pages = [str(uuid.uuid4()).split('-')[0] for _ in range(random.randint(1, n_pages))]
@@ -16,6 +17,13 @@ def worker():
 
 
 if __name__ == '__main__':
+    
+    # Recover state
+    #if os.path.exists('transactions'):
+    manager = Manager()
+    manager.recover()
+   
+    '''
     pool = [threading.Thread(target=worker) for _ in range(n_workers)]
     map(lambda t: t.start(), pool)
-
+    '''
--- Manager.py
+++ Manager.py
@@ -3,6 +3,7 @@ import time
 import logging
 import hashlib
 import threading
+import os
 
 # Implement manager class with persitency, buffer, Singleton and all the good stuff
 class Singleton(type):
@@ -21,11 +22,12 @@ class Manager(object):
     lsn = 0
     buffer_size = 0
     lock = threading.Lock()
-    
+
     def __init__(self):
-        self.logger = self.initialize_writer('transactions')
-        self.writer = self.initialize_writer('commits')
- 
+        with Manager.lock:
+            self.logger = self.initialize_writer_log('transactions')
+        #self.writer = self.initialize_writer('data')
+
     def begin_transaction(self):
         with Manager.lock:
             taid = uuid.uuid4()
@@ -55,7 +57,7 @@ class Manager(object):
             
             # If more then 5 datasets, in this case 'writes', are in the buffer then propagate to storage
             if Manager.buffer_size > 5:
-                self.propagate()
+                self.__propagate()
         '''
         self.pending_transactions[taid][page_id].append({'lsn':lsn.hexdigest(), 'data':data})
         self.logger.info('{lsn}, {taid}, {page_id}, {data}'.format(lsn=lsn.hexdigest(),
@@ -66,19 +68,73 @@ class Manager(object):
 
     # Commit transaction in the buffer
     def commit(self, taid):
-        self.commited_transactions.append(taid)
+        with Manager.lock:
+            Manager.lsn += 1
+            self.logger.info('{lsn}, {taid}, {redo}'.format(lsn=self.lsn,
+                                                            taid=taid,
+                                                            redo=True))
+            Manager.commited_transactions.append(taid)
              
     # Propagate commited transaction in the physical storage
-    def propagate(self):
-        for taid in self.commited_transactions:
-            for page_id in self.pending_transactions[taid]:
-                data = self.pending_transactions[taid][page_id][-1]
-                self.writer.info('{lsn}, {page_id}, {data}'.format(page_id=page_id, **data))
-                Manager.buffer_size -= len(self.pending_transactions[taid][page_id])
-            del self.pending_transactions[taid]
-            logging.info('pending transactions after commit: {0}'.format(self.pending_transactions))
-        del self.commited_transactions[:]
+    def __propagate(self):
+        for taid in Manager.commited_transactions:
+            for page_id in Manager.pending_transactions[taid]:
+                writer = self.initialize_writer('data/' + str(page_id))
+                data = Manager.pending_transactions[taid][page_id][-1]
+                writer.info('{lsn}, {page_id}, {data}'.format(page_id=page_id, **data))
+                Manager.buffer_size -= len(Manager.pending_transactions[taid][page_id])
+            del Manager.pending_transactions[taid]
+            logging.info('pending transactions after commit: {0}'.format(Manager.pending_transactions))
+        del Manager.commited_transactions[:]
+    
+    # Recover state before crash
+    def recover(self):
+        # Load logfile into memory
+        with open('transactions', 'r') as data_file:
+            if os.stat('transactions').st_size == 0:
+                return
+            else:
+                read_lines = data_file.readlines()
+                transactions = [(line.rstrip('\n')).split(',') for line in read_lines]
+                # Recover LSN
+                Manager.lsn = int(transactions[-1][0])
+        
+        # Find winner transactions
+        winners_ta = self.__find_ta_winners(transactions)
+        # Read log life sequentially and redo needed transactions
+        self.__redo_winners(transactions, winners_ta)
 
+    def __find_ta_winners(self, transactions):
+        winners_ta = []
+        for TA in transactions:
+            if len(TA) < 4:
+                winners_ta.append(TA[1].strip()) 
+        return winners_ta
+
+    def __redo_winners(self, transactions, winners_ta):
+        for TA in transactions:
+            if len(TA) > 3:
+                log_lsn = int(TA[0])
+                taid = TA[1].strip()
+                page_id = TA[2].strip()
+                data = TA[3].strip()
+                if os.path.exists(page_id):
+                    # Find LSN of page
+                    page_lsn = self.__load_page_lsn(page_id)  
+                else:
+                    page_lsn = 0
+                # Find write to redo
+                if taid in winners_ta and log_lsn  > page_lsn:
+                    with open('data/' + page_id, 'w') as fp:
+                        record = '{lsn}, {page_id}, {data}'.format(lsn = log_lsn, page_id = page_id, data = data)
+                        record += '\n'
+                        fp.write(record)
+
+    def __load_page_lsn(self, pageid):
+        page_lsn = 0
+        with open('data/' + pageid, 'r') as data_file:
+            read_lines = data_file.readlines()
+        return int(read_lines[0].rstrip('\n').split(',')[0])
 
     @staticmethod
     def initialize_writer(file_name):
@@ -90,7 +146,23 @@ class Manager(object):
 
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
+        
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+        return logger
 
+    @staticmethod
+    def initialize_writer_log(file_name):
+        logger = logging.getLogger(file_name)
+        formatter = logging.Formatter('%(message)s')
+
+        file_handler = logging.FileHandler(file_name, mode='a+')
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        
         logger.setLevel(logging.DEBUG)
         logger.addHandler(file_handler)
         logger.addHandler(stream_handler)
